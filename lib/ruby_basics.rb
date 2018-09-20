92fa376c439b6e5b41dfa6e9f452891cb0c076ba
Virginia Dooley <vcdooley@gmail.com>
2018-09-20 08:44:42 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -10,29 +10,12 @@ def argue
   
 end
 
-def greeting(greeting, name"Hi there", "Bobby!")
-  return 
+def greeting(greeting, name)
+  return #{greeting} + {#name}
 end
 
-# describe '#greeting' do
-#     it 'takes two arguments' do
-#       greeting = "Hi there, "
-#       name = "Bobby!"
+greeting("Hi there, ", "Bobby!")
 
-#       expect{ greeting(greeting, name) }.to_not raise_error
-#     end
-
-#     it 'raises an error with one argument' do
-#       greeting = "Hi there, "
-
-#       expect{ greeting(greeting)}.to raise_error
-#     end
-#   end
-
-
-
-
-  end
 
 def return_a_value
 
